7a91ff50adb073980134546e9ee62ff41da76959
prclima <pedro@futuranet.com.br>
2022-11-22 10:39:12 -0300

    lista mercado

--- lista-de-compras/src/components/formulario/index.js
+++ lista-de-compras/src/components/formulario/index.js
@@ -1,16 +1,27 @@
 import { useState } from "react";
 import axios from "axios";
+import Col from 'react-bootstrap/Col';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
 
 function Formulario() {
   const [form, setForm] = useState({
     nomeLista: "",
     telefone: 0,
     nomeMercado: "",
-    produtos: {},
+    produtos: [],
   });
 
   function HandleChange(e) {
-    setForm({ ...form, [e.target.name]: e.target.value });
+      if (e.target.name === "nomeMercado"){
+        setForm({ ...form, nomeMercado : e.target.selected})
+      }
+
+    setForm({ ...form, [e.target.name]: e.target.value })
+    console.log(e.target);
   }
 
   async function HandleSubmit(e) {
@@ -31,45 +42,56 @@ function Formulario() {
 
   return (
     <>
+   
     <h1> Dados da Lista</h1>
-    <form>
-      <label htmlFor="nomeLista">Nome da Lista:</label>
-      <input
-        name="nomeLista"
-        type="text"
-        value={form.nomeLista}
-        placeholder="Informe o nome da lista"
-        onChange={HandleChange}
-      />
-
-      <label htmlFor="telefone"> Telefone</label>
-      <input
-        placeholder="Informe o telefone para contato"
-        value={form.telefone}
-        name="telefone"
-        onChange={HandleChange}
-      />
-
-      <label htmlFor="nomeMercado"> Nome do Mercado</label>
-      <input
-        type="text"
-        placeholder="Informe o mercado de preferencia"
-        value={form.nomeMercado}
-        name="nomeMercado"
-        onChange={HandleChange}
-      />
-
-      <label htmlFor="produtos"> Produtos</label>
-      <input
-        type="text"
-        placeholder="Faça sua lista"
-        name="produtos"
-        onChange={HandleChange}
-      />
-
-      <button onClick={HandleSubmit}>Enviar</button>
-    </form>
-    </>
-  );
+    <Form>
+    <Row className="col-md-9">
+    <Stack direction="horizontal" gap={3}>
+      <Col>
+       
+        <FloatingLabel  controlId="nomeLista" label="Informe o nome da lista" >
+          <Form.Control type="text"   name="nomeLista" value={form.nomeLista} onChange={HandleChange}/>
+        </FloatingLabel>
+      </Col>
+      <Col >
+        <FloatingLabel controlId="telefone" label="Informe o telefone para contato" >
+          <Form.Control type="number" name="telefone" value={form.telefone} onChange={HandleChange}/>
+        </FloatingLabel>
+      </Col>
+      <Col >
+        <Form.Select aria-label="Informe o mercado de preferencia" controlId="nomeMercado" onChange={HandleChange} name="nomeMercado"  >
+      <option selected >Mercados</option>
+      <option value="Pão de Açucar">Pão de Açucar</option>
+      <option value="Extra">Extra</option>
+      <option value="Mambo">Mambo</option>
+      <option value="Natural da Terra">Natural da Terra</option>
+      <option value="Mercadao">Mercadão</option>
+      <option value="Mercado da Liberdade">Mercado da Liberdade</option>
+      <option value="Eataly">Eataly</option>
+      <option value="Hirota">Hirota</option>
+      <option value="Assai">Assai</option>
+      <option value="Atacadão">Atacadão</option>
+      <option value="Makro">Makro</option>
+      <option value="Maxxi Atacados">Maxxi Atacados</option>
+      <option value="Tenda Atacados">Tenda Atacados</option>
+      <option value="Sams Club">Sams Club</option>
+      <option value="Spani Atacados">Spani Atacados</option>
+      <option value="Carrefour">Carrefour</option>
+      <option value="Dia">Dia</option>
+      <option value="Walmart">Walmart</option>
+      <option value="Outro">Outro</option>
+    </Form.Select>
+      </Col>
+      <Col md>
+        <FloatingLabel controlId="produtos" label="Faça sua lista" >
+          <Form.Control type="text" name="produtos" onChange={HandleChange}/>
+        </FloatingLabel>
+      </Col>
+      <Button variant="primary" type="submit" onClick={HandleSubmit}>Enviar</Button>
+      </Stack>
+    </Row>
+</Form>
+</>
+  )
 }
 export default Formulario;
